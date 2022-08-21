56ea012a337be19fed71bc4f24a862642f77b9b2
Dmitrii Esin <dmitrii_esin@epam.com>
2022-08-21 13:24:39 -0400

    task added

--- algorithms/stack.js
+++ algorithms/stack.js
@@ -58,3 +58,57 @@ st.push(3);
 st.pop();
 
 console.log("!!st.getMinimum() === 1", st.getMinimum());
+
+// TODO:Stack of Plates: Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure SetOfStacks that mimics this. SetOfStacks should be composed of several stacks and should create a new stack once the previous one exceeds capacity.
+
+class StackOfStacks {
+  stacks = [];
+  capacity = 3;
+
+  constructor(capacity = 3) {
+    this.capacity = capacity;
+  }
+
+  push(data) {
+    if (this.stacks.length === 0) {
+      this.stacks.push([data]);
+    } else {
+      const lastStack = this.stacks[this.stacks.length - 1];
+
+      if (lastStack.length === this.capacity) {
+        this.stacks.push([data]);
+      } else {
+        lastStack.push(data);
+      }
+    }
+  }
+
+  pop() {
+    if (this.stacks.length === 0) throw new Error("Stack is empty");
+
+    const lastStack = this.stacks[this.stacks.length - 1];
+    const val = lastStack.pop();
+
+    if (lastStack.length === 0) {
+      this.stacks.pop();
+    }
+
+    return val;
+  }
+}
+
+const stOfSt = new StackOfStacks();
+
+stOfSt.push(1);
+stOfSt.push(2);
+stOfSt.push(3);
+stOfSt.push(4);
+stOfSt.push(5);
+stOfSt.push(6);
+stOfSt.push(7);
+stOfSt.push(8);
+
+console.log("!!stOfSt", stOfSt); /// !!stOfSt StackOfStacks {
+//   stacks: [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8 ] ],
+//   capacity: 3
+// }
